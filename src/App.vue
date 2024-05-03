c767dfa55a31efd6c7f91bc25541da16904ec073
alaminrahman <sumonrahman3600@gmail.com>
2024-05-03 11:43:43 +0600

    Jobs searching done

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+  <header>
+    <div class="order">
+      <button @click="handleClick('title')">Order by title</button>
+      <button @click="handleClick('salary')">Order by salary</button>
+      <button @click="handleClick('location')">Order by location</button>
+    </div>
+  </header>
+
+   <JobList :jobs="jobs" :order="order"/>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, ref } from 'vue';
+import JobList from './components/JobList.vue';
+import Job from './types/Job';
+import OrderTerm from './types/OrderTerm';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components: { JobList },
+
+  setup() {
+
+    const jobs = ref<Job[]>([
+      { title: 'Farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+      { title: 'Quarryman', location: 'death mountain', salary: 50000, id: '2' },
+      { title: 'flute player ', location: 'lon lost wood', salary: 20000, id: '3' },
+      { title: 'fisherman', location: 'lake hylia', salary: 10000, id: '4' },
+      { title: 'prison guard', location: 'gerudo valley', salary: 660000, id: '5' },
+    ])
+
+    const order = ref<OrderTerm>('title')
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return { jobs, handleClick, order }
+
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
+
 </style>
